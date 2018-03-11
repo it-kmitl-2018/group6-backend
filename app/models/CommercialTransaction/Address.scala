475a20a612d0c28e8cb585a37989ee147f7354ab
pikepeak <khunach@msn.com>
2018-03-12 00:35:58 +0700

    deleted parameter addressLine1 and addressLine2 in class Address

--- app/models/CommercialTransaction/Address.scala
+++ app/models/CommercialTransaction/Address.scala
@@ -3,8 +3,6 @@ package models.CommercialTransaction
 case class Address(
     postalCode: String,
     buildingName: String,
-    addressLine1: String,
-    addressLine2: String,
     lane: String,
     villageName: String,
     villageNumber: String,
