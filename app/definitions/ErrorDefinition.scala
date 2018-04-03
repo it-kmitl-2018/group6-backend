ce6b17dbad40a72ca0228b4eca650d8286cbb21a
Taweewong Tocharoen <taweewong.to@gmail.com>
2018-04-03 17:08:25 +0700

    created object ErrorDefinition

---
+++ app/definitions/ErrorDefinition.scala
@@ -0,0 +1,7 @@
+package definitions
+
+object ErrorDefinition {
+  val BAD_JSON_FORMAT = "Bad Json Format"
+  val NOT_JSON_REQUEST = "Request is not a json"
+  val EMPTY_JSON_REQUEST = "Request is Empty"
+}
