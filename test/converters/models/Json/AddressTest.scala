e1feb47dad4ee414d1dc12a560982a3883ac257e
pikepeak <khunach@msn.com>
2018-04-10 11:47:43 +0700

    created test AddressTest

---
+++ test/converters/models/Json/AddressTest.scala
@@ -0,0 +1,59 @@
+package converters.models.Json
+
+import org.scalatest.FunSuite
+
+class AddressTest extends FunSuite {
+  val lineOne = "สำนักงานใหญ่ เลขที่ 313/1"
+  val lineTwo = "ถนนรัชดาภิเษก แขวงบุคลโล เขตธนบุรี กรุงเทพมหานคร 10600"
+  val cityName = "ธนบุรี"
+  val citySubDivisionName = "บุคคโล"
+  val postCode = "10600"
+  val countrySubDivision = "กรุงเทพมหานคร"
+  val countryName = "TH"
+  val address = Address(
+      lineOne,
+      lineTwo,
+      cityName,
+      citySubDivisionName,
+      postCode,
+      countrySubDivision,
+      countryName)
+
+  test("lineOne in Address constructed is correctly") {
+    assert(address.lineOne === lineOne)
+  }
+
+  test("lineTwo in Address constructed is correctly") {
+    assert(address.lineTwo === lineTwo)
+  }
+
+  test("cityName in Address constructed is correctly") {
+    assert(address.cityName === cityName)
+  }
+
+  test("citySubDivisionName in Address constructed is correctly") {
+    assert(address.citySubDivisionName === citySubDivisionName)
+  }
+
+  test("postCode in Address constructed is correctly") {
+    assert(address.postCode === postCode)
+  }
+
+  test("countrySubDivision in Address constructed is correctly") {
+    assert(address.countrySubDivision === countrySubDivision)
+  }
+
+  test("countryName in Address constructed is correctly") {
+    assert(address.countryName === countryName)
+  }
+
+  test("Address constructed is correctly") {
+    assert(address.lineOne === lineOne)
+    assert(address.lineTwo === lineTwo)
+    assert(address.cityName === cityName)
+    assert(address.citySubDivisionName === citySubDivisionName)
+    assert(address.postCode === postCode)
+    assert(address.countrySubDivision === countrySubDivision)
+    assert(address.countryName === countryName)
+  }
+}
