93e9e9ddccefcc7181be124b2974f168eaab8072
Taweewong Tocharoen <taweewong.to@gmail.com>
2018-04-11 18:17:28 +0700

    deleted hard code mock data and get mock data from object MockData

--- app/mocks/converters/TaxInvoiceConverterMock.scala
+++ app/mocks/converters/TaxInvoiceConverterMock.scala
@@ -1,22 +1,14 @@
 package mocks.converters
 
 import javax.inject.Singleton
-import models.CommercialTransaction.Address
-import models.TradeAgreement.{Dealer, ReferenceDocument, TradeAgreement}
+import mocks.data.MockData
+import models.TradeAgreement.TradeAgreement
 import play.api.libs.json.JsValue
 import utilities.Json
 
 @Singleton
 class TaxInvoiceConverterMock {
   def convertTradeAgreementToJson(tradeAgreement: TradeAgreement): JsValue = {
-    val orderReference = ReferenceDocument("id1", 1234567890, "CODE1")
-    val additionalReference = ReferenceDocument("id1", 1234567890, "CODE2")
-    val deliveryTypeCode = "D123"
-    val address = Address("123", "building", "lane", "village", "456",
-      "sukhumvit", "district", "asjdh", "thailand", "bangkok", "789")
-    val dealer = Dealer("dealerId", "globalId", "name", "taxPayerId", "uri", "email", address)
-    val tradeAgreement = TradeAgreement(dealer, dealer, deliveryTypeCode, orderReference, additionalReference)
-
-    Json.toJson(tradeAgreement)
+    Json.toJson(MockData.getMockTradeAgreement)
   }
 }
