40fd7aa366cc2d72074d0afdb9671e9dbf2eb567
Taweewong Tocharoen <taweewong.to@gmail.com>
2018-04-11 18:08:48 +0700

    changed function getTradeAgreementAsJson returning type from String to JsValue

--- app/facades/TaxInvoiceFacade.scala
+++ app/facades/TaxInvoiceFacade.scala
@@ -3,10 +3,11 @@ package facades
 import javax.inject.{Inject, Singleton}
 import mocks.converters.TaxInvoiceConverterMock
 import models.TradeAgreement.TradeAgreement
+import play.api.libs.json.JsValue
 
 @Singleton
 class TaxInvoiceFacade @Inject()(taxInvoiceConverterMock: TaxInvoiceConverterMock) {
-  def getTradeAgreementAsJson(tradeAgreement: TradeAgreement): String = {
+  def getTradeAgreementAsJson(tradeAgreement: TradeAgreement): JsValue = {
     taxInvoiceConverterMock.convertTradeAgreementToJson(tradeAgreement)
   }
 }
