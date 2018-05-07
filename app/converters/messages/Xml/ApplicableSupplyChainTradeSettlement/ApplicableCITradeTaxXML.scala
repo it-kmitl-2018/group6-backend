d30627b888cbd4ce53a0e96c58f49b428d0f0b16
pikepeak <khunach@msn.com>
2018-05-07 20:58:52 +0700

    created message ApplicableCITradeTaxXML

---
+++ app/converters/messages/Xml/ApplicableSupplyChainTradeSettlement/ApplicableCITradeTaxXML.scala
@@ -0,0 +1,6 @@
+package converters.messages.Xml.ApplicableSupplyChainTradeSettlement
+
+case class ApplicableCITradeTaxXML(
+    typeCode: String,
+    calculatedRate: Int,
+    basisAmount: Double)
