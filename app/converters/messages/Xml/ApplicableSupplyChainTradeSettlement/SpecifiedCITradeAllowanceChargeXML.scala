d47b6c4d1ce09c671dec970f91c902d08bc8cb95
pikepeak <khunach@msn.com>
2018-05-07 21:05:15 +0700

    created message SpecifiedCITradeAllowanceChargeXML

---
+++ app/converters/messages/Xml/ApplicableSupplyChainTradeSettlement/SpecifiedCITradeAllowanceChargeXML.scala
@@ -0,0 +1,6 @@
+package converters.messages.Xml.ApplicableSupplyChainTradeSettlement
+
+case class SpecifiedCITradeAllowanceChargeXML(
+    chargeIndicator: Boolean,
+    actualAmount: Double,
+    TypeCode: String)
