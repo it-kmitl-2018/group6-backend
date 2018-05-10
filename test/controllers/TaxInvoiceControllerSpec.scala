43c87954a3b7f8e9f68a1442168470033be74706
Taweewong Tocharoen <taweewong.to@gmail.com>
2018-05-10 20:12:45 +0700

    edited api convert json test in TaxInvoiceControllerSpec

--- test/controllers/TaxInvoiceControllerSpec.scala
+++ test/controllers/TaxInvoiceControllerSpec.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import converters.TaxInvoiceJsonConverter
 import mocks.data.MockData
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
@@ -8,15 +9,19 @@ import play.api.test._
 import utilities.Json
 
 class TaxInvoiceControllerSpec extends PlaySpec with GuiceOneAppPerTest {
+  private val taxInvoiceJsonConverter = new TaxInvoiceJsonConverter()
+
   "POST /tax_invoice/json" should {
     "return tax invoice json" in {
       val json = Json.toJson(MockData.getMockTradeAgreement)
+      val jsonResponse = taxInvoiceJsonConverter
+        .convertTradeAgreementToJson(MockData.getMockTradeAgreement)
       val request = FakeRequest(POST, "/tax_invoice/json").withJsonBody(json)
       val result = route(app, request).get
 
       status(result) mustBe OK
       contentType(result) mustBe Some("application/json")
-      contentAsString(result) must include(json.toString())
+      contentAsString(result) must include(jsonResponse.toString())
     }
   }
 }
