42fcd2479afeaec2e5dce7d4342f5660a6400d4d
Taweewong Tocharoen <taweewong.to@gmail.com>
2018-05-10 20:17:32 +0700

    created function convertTradeAgreementToXML in TaxInvoiceConverterMock

--- app/mocks/converters/TaxInvoiceConverterMock.scala
+++ app/mocks/converters/TaxInvoiceConverterMock.scala
@@ -6,9 +6,15 @@ import models.TradeAgreement.TradeAgreementForm
 import play.api.libs.json.JsValue
 import utilities.Json
 
+import scala.xml.Elem
+
 @Singleton
 class TaxInvoiceConverterMock {
   def convertTradeAgreementToJson(tradeAgreement: TradeAgreementForm): JsValue = {
     Json.toJson(MockData.getMockTradeAgreement)
+  }
+
+  def convertTradeAgreementToXML(tradeAgreement: TradeAgreementForm): Elem = {
+    MockData.getMockSimpleXML
   }
 }
