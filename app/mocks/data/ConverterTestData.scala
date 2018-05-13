7e74d3e3f52fda760be0c6485ddd20fbb04e327a
pikepeak <khunach@msn.com>
2018-05-13 19:06:15 +0700

    fixed transactionDate parameter value from String to Long Type in converters and mockdata

--- app/mocks/data/ConverterTestData.scala
+++ app/mocks/data/ConverterTestData.scala
@@ -10,7 +10,7 @@ object ConverterTestData {
   private val seller = Seller("taxPayerId", "", "", "name", "email", "", "", address)
   private val buyer = Buyer("taxPayerId", "", "", "name", "email", "", "", address)
   private val number = ""
-  private val transactionDate = ""
+  private val transactionDate = 0
   private val note = Note("หมายเหตุ", "")
   private val items = new ListBuffer[Item]
   val taxInvoice = TaxInvoice(number, seller, buyer, transactionDate, note, items)
